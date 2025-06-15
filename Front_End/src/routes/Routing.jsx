19b2de73c9dcb06f59cb0693df2e69bbfc911e4a
khris-47 <74474330+khris-47@users.noreply.github.com>
2025-06-15 15:22:18 -0600

    ,,

--- Front_End/src/routes/Routing.jsx
+++ Front_End/src/routes/Routing.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 
 import Index from '../pages/principales/index'
-import M_Juegos from '../pages/menus/M_Juegos'
-import Juego01 from '../pages/juegos/Juego01'
-import Juego02 from '../pages/juegos/Juego02'
-import Juego03 from '../pages/juegos/Juego03'
 import Quienes_somos from '../pages/principales/quienes_somos'
 import Contacto from '../pages/principales/Contacto'
 import Login from '../pages/principales/Login'
@@ -29,10 +25,6 @@ function Routing() {
         <Routes>
         <Route path="/" element={<Navigate to="/index" />} />
             <Route path='/index' element={<Index/>}></Route>
-            <Route path='/menu_juegos' element={<M_Juegos/>}></Route>
-            <Route path='/juego01' element={<Juego01/>}></Route>
-            <Route path='/juego02' element={<Juego02/>}></Route>
-            <Route path='/juego03' element={<Juego03/>}></Route>
             <Route path='/about' element={<Quienes_somos/>}></Route>
             <Route path='/contact' element={<Contacto/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
