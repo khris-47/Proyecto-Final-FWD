7b72b8c4678fd417fcab88fdecfc179aba21d59e
Khris_Rod <rodriguezchris808@gmail.com>
2025-06-15 13:01:36 -0600

    responsividad del footer

--- Front_End/src/components/principales/About_content.jsx
+++ Front_End/src/components/principales/About_content.jsx
@@ -3,10 +3,11 @@ import Footer from '../navegacion/Footer'
 import NavBar from '../navegacion/navBar'
 import Fondo from '../../assets/img/fondos/fondo_AboutUs.png'
 import '../../styles/About.css'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
 function About_content() {
 
+    const navigate = useNavigate()
 
     return (
         <div className="bodyAbout">
@@ -31,11 +32,11 @@ function About_content() {
                 <main className="main-content">
                     <div className="section izquierda">
                         <h1>Entre Olas y </h1>
-                        <h1 style={{ color: 'aqua'}}>Manglares</h1>
+                        <h1 style={{ color: '#0094ff'}}>Manglares</h1>
                         <p>
                             El proyecto TC-782 Gestión del patrimonio cultural en comunidades costeras e insulares, dirige su accionar a la protección y visibilización del patrimonio cultural material e inmaterial, esencial para preservar la historia e identidad de los pueblos, y para transmitir este legado a las generaciones futuras.
                         </p>
-                        <Link to='/contact' className='btn btn-info'>Contáctanos</Link>
+                        <button onClick={() => navigate('/contact')} className='contacto-btn'>Contáctanos</button>
                     </div>
 
                     <div className="section derecha-transparente"></div>
